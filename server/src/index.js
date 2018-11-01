03bc24600e906e8cdc4bdea48655f47aca6bbded
Devin Eldreth <devin.eldreth@gmail.com>
2018-11-01 15:37:18 -0400

    Bases

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,24 @@ const { importSchema } = require("graphql-import");
 const { Prisma } = require("prisma-binding");
 const path = require("path");
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    locations: (obj, args, context, info) => {
+
+    },
+    location: (obj, args, context, info) => {
+
+    }
+  },
+  Mutation: {
+    addLocation: (obj, args, context, info) => {
+
+    },
+    addCat: (obj, args, context, info) => {
+      
+    }
+  }
+};
 
 const typeDefs = importSchema(path.resolve("src/schema.graphql"));
 
